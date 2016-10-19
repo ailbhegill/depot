8f96edf32496adfd3a1d94075db3f2ce2e091969
ailbhegill <ailbhe.g@hotmail.com>
2016-10-19 23:38:37 +0100

    Inserted up and down methods into combine_items_in_cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019214716) do
+ActiveRecord::Schema.define(version: 20161019220642) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
